3108d1aa0013ebffa499283b0b1e1183c60f2350
d6nnish <S10247511@connect.np.edu.sg>
2022-08-02 11:36:13 +0800

    lol

--- mainSolution.py
+++ mainSolution.py
@@ -1,3 +1,6 @@
+from http.client import GATEWAY_TIMEOUT
+
+
 <<<<<<< HEAD
 hello lol
 dmdwkd
@@ -10,3 +13,4 @@ ihij
 =======
 >>>>>>> 87e49716d6443bed09c150268a7943f407841b01
 >>>>>>> 5f6b7d7937ab32340146b139794da67753a8be69
+makkau GAy 
