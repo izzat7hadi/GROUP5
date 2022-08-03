0c9d6577c7effa5f939a2060102da5b9cc8f34fa
ReneeLeron <S10240362@connect.np.edu.sg>
2022-08-03 13:01:10 +0800

    g

---
+++ profit_loss.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+import csv
+
+pal=Path.cwd()/"csv_reports"/"profit-and-loss-usd.csv"
+with pal.open(mode="r",encoding="UTF-8") as file:
+    reader=csv.reader(file)
+    for line in reader:
+        print(line)
+        
