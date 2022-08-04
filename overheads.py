796e0e2e810b87855b1b0c1e00b421d12bcb81bc
d6nnish <S10247511@connect.np.edu.sg>
2022-08-04 18:33:18 +0800

    heya babies

--- overheads.py
+++ overheads.py
@@ -6,25 +6,19 @@ import csv
 from pickle import APPEND
 import string
 from unicodedata import category
-
+#to open files from folder
 oh=Path.cwd()/"csv_reports"/"overheads-day-45.csv"
-
+#using reading mode to read files
 with oh.open(mode="r", encoding="UTF-8") as file:
-<<<<<<< HEAD
-    empty_list=[]
-    category=[]
-    reader=csv.reader(file)
-    next(reader)
-    
-=======
     overheads=[]
     category=[]
     reader=csv.reader(file)
     next (reader)
     for line in reader:
+        #using append to add elements to the list
         category.append(line[0])
         overheads.append(line[1])
-        
+ #creating new list to find       
 highest=[]
 for value in overheads:
     value=float(value)
@@ -36,8 +30,7 @@ def check():
         if each>largest:
             largest=each
             index=(highest.index(largest))
-    print('[HIGHEST OVERHEAD]',category[index],',',largest)
+    print(f"[HIGHEST OVERHEAD] {category[index]} : {largest} %")
 
 check()
 
->>>>>>> 2d04d4ac051d747a6fce75df80706c3d7f8b60d1
