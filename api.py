678d706d42fbbb7e1de5383e7ded718074be239f
ReneeLeron <S10240362@connect.np.edu.sg>
2022-08-04 21:19:34 +0800

    g

--- api.py
+++ api.py
@@ -6,48 +6,19 @@ from textwrap import indent
 from tkinter import Button
 from urllib import response
 
-#import requests
-#api_keys = "QL8Q3ZVY82Y2DS1F"
-#url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey={api_keys}"
-#response = requests.get(url)
-#info=response.json()
-
-#print(info)
-
-#print (response)
-#print(response.headers)
-#print(response.headers.get('Content-Type'))
-#data = response.json()
-#print(type(data))
-#print(data.keys())
-#print(data)
-#print(data["region_metadata"])
-#print(data["items"])
-#print(data["api_info"])
-#import json
-# print(json.dumps(data["items"],indent=4))
-#pm25 = data["items"]
-#print(len(pm25))
-#pm_list = []
-#for item in pm25:
-    #print (item)
-    #item["readings"]["pm25_one_hourly"].update({"timestamp": item["timestamp"]})
-    #print(item["readings"]["pm25_one_hourly"])
-    #pm_list.append(item["readings"]["pm25_one_hourly"])
-    #item["readings"]["pm25_one_hourly"].update({"timestamp": item["timestamp"]})
-   # pm_list.append(item["readings"]["pm25_one_hourly"])
-#print(pm_list)
-
 import requests
 
 def api_extractor() :
 
+#making the api key as a variable so that the code could access to the data
     api_keys = "QL8Q3ZVY82Y2DS1F"
+    #inserting the url as a variable and including the api key to access the data
     url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=" + api_keys
+    #making a response variable 
     response = requests.get(url)
     info = response.json()
-    last_week_date = list (info['Time Series FX (Weekly)'].keys() [0])
-    last_week_closing = info ['Time Series FX (Weekly)'] [last_week_date] ['4.close']
+    last_week_date  = list(info['Time Series FX (Weekly)'].keys())[0]
+    last_week_closing = info['Time Series FX (Weekly)'][last_week_date]['4. close']
     return float(last_week_closing)
 
 print(api_extractor())
