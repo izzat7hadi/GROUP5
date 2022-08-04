bd6a4b12b0559db8136bd37bb9a4db669457122e
izzat7hadi <S10242578@connect.np.edu.sg>
2022-08-04 17:08:36 +0800

    jj

--- overheads.py
+++ overheads.py
@@ -1,37 +1,17 @@
 import csv
+from hashlib import new
 from operator import index
 from pathlib import Path 
 import csv
+from pickle import APPEND
+import string
 from unicodedata import category
 
-pal=Path.cwd()/"csv_reports"/"overheads-day-45.csv"
+oh=Path.cwd()/"csv_reports"/"overheads-day-45.csv"
 
-with pal.open(mode="r", encoding="UTF-8") as file:
+with oh.open(mode="r", encoding="UTF-8") as file:
     empty_list=[]
+    category=[]
     reader=csv.reader(file)
-    for line in reader:
-        empty_list.append(line)
-<<<<<<< HEAD
-
-def main(data):
-    highest=[]
-
-    category = list(index[0] for index in data[1:])
-    overheads = list((index[1]) for index in data [1:])
-    highest.append([category,overheads])
-   
-    for index in range(len(category)):
-     if (index==0):
-            continue 
-     largestvalue= -1
+    next(reader)
     
-    for each in overheads :
-          if each > largestvalue :
-            largestvalue = each
-            highest.append([overheads,largestvalue ])
-    print(each)
-main(empty_list) 
-=======
-    print(line)
->>>>>>> 850e156af8ee6ca43f5fc6cd0726930ce4aaf242
-
