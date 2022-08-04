c8a8e4c93c5c820903e46d1afe7e4b22e80f2184
izzat7hadi <S10242578@connect.np.edu.sg>
2022-08-04 21:47:29 +0800

    jk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import api, cash_on_hand, overheads, profit_loss
 from pathlib import Path
 import csv
-
+usd=api.api_extractor()
+print(usd)
 def main():
     forex=api.api_extractor()
     cash_on_hand.Daily_checker(forex)
@@ -11,12 +12,16 @@ def main():
 main()
 final=Path.cwd()/"final.csv"
 final.touch
+#open file to read and return the data
 with final.open(mode="a",encoding="UTF-8",newline="") as file:
+    #adding the extracted data into empty list by appending and extending
+    
+    #append is for non-strings,extend is for strings
     empty_list=[]
     empty_list.append(cash_on_hand)
     empty_list.append(profit_loss)
     empty_list.append(overheads)
-    
+    #adding the data to the newly created csv file
     writer=csv.writer(file)
     writer.writerow(empty_list)
     file.close()
