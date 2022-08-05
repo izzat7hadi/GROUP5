d3028141b893f6b4dc2408340ab05fc10b1f6b90
ReneeLeron <S10240362@connect.np.edu.sg>
2022-08-05 20:37:05 +0800

    f

--- api.py
+++ api.py
@@ -14,13 +14,17 @@ def api_extractor() :
     api_keys = "QL8Q3ZVY82Y2DS1F"
     #inserting the url as a variable and including the api key to access the data
     url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol=USD&to_symbol=SGD&apikey=" + api_keys
-    #making a response variable 
+    #making a response variable to send request to get data from the url
     response = requests.get(url)
+    #keeping the response which is a json data as info
     info = response.json()
+    #making a variable for last weeks' date as list with info for time series fx weekly with api key
     last_week_date  = list(info['Time Series FX (Weekly)'].keys())[0]
+    #making a variable for last week closing as info with time series fx weekly together with last weeks' date and the header for closing price in data
     last_week_closing = info['Time Series FX (Weekly)'][last_week_date]['4. close']
+    #calling out the variable as a float
     return float(last_week_closing)
-
+#calling out the entire code
 print(api_extractor())
 
 
